58b86508e55e147fc8055a28ae27db740097038d
Miguel de Icaza <miguel@gnome.org>
2023-12-31 09:21:14 -0500

    Initial Commit

---
+++ GodotSnippetEditor/GodotSnippetEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GodotSnippetEditorApp.swift
+//  GodotSnippetEditor
+//
+//  Created by Miguel de Icaza on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct GodotSnippetEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
